f6bcc025bcdc7181b6fdb0510750c0b5072e3034
Alexander Klimov <klimovsanya01@gmail.com>
2019-12-26 05:45:10 +0300

    ready func

--- funcs.cpp
+++ funcs.cpp
@@ -1,22 +1,4 @@
-//funcs.cpp
-
-#include "funcs.h"
-#include <cmath>
-double MySum(double a, double b)
-{
-	return a + b;
-}
-double MySub(double a, double b)
-{
-	return a - b;
-}
-
 double MyMul(double a, double b)
 {
 	return a * b;
-}
-
-double MyDiv(double a, double b)
-{
-	return a / b;
 }
