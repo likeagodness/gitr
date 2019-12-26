6995a7977577774b95109ff1240d7336ad35bae3
Alexander Klimov <klimovsanya01@gmail.com>
2019-12-26 04:38:47 +0300

    add main first

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int  b, c, d;
+
+	cout << "Выберите операцию" << endl;
+	cout << "1. Сложить" << endl << "2. Вычесть" << endl;
+	cout << "3. Умножить" << endl << "4. Деление" << endl;
+	cin >> b;
+	cout << endl;
+
+	switch (b)
+	{
+	case 1:
+		// Здесь будет код
+		
+	case 2:
+		// Здесь будет код
+		
+	case 3:
+		// Здесь будет код
+		
+	case 4:
+		// Здесь будет код
+		
+
+	}
+	system("pause");
+	return 0;
+}
+
